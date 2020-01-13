ab324992b5922336007ed3b7d5372b1c51d02eb5
Laxman Bhattarai <erluxman@gmail.com>
2020-01-13 10:49:37 +0545

    fixed failing test, my bad copy paste with blind eyes

--- lib/utils/extensions/datetime.dart
+++ lib/utils/extensions/datetime.dart
@@ -3,8 +3,8 @@ import 'package:productive_app/utils/extensions/numbers.dart';
 extension DateTimeExtensions on DateTime {
   String toStringYYYYMMDD({String sep: "-"}) {
     String year = this.year.toString();
-    String month = this.year.toStringTwoDigits();
-    String day = this.year.toStringTwoDigits();
+    String month = this.month.toStringTwoDigits();
+    String day = this.day.toStringTwoDigits();
     return"$year$sep$month$sep$day";
   }
 }
