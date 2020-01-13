cb573737ef1d4a46927e513c8d66f5559f07ffe0
Laxman Bhattarai <erluxman@gmail.com>
2020-01-13 10:27:36 +0545

    added linter

--- lib/utils/extensions/datetime.dart
+++ lib/utils/extensions/datetime.dart
@@ -1,7 +1,7 @@
 import 'package:productive_app/utils/extensions/numbers.dart';
 
 extension DateTimeExtensions on DateTime {
-  String toStringYYYYMMDD({String separator: "-"}) {
-    return "${this.year.toString()}$separator${this.month.toStringTwoDigits()}$separator${this.day.toStringTwoDigits()}";
+  String toStringYYYYMMDD({String sep: "-"}) {
+    return "${this.year.toString()}$sep${this.month.toStringTwoDigits()}$sep${this.day.toStringTwoDigits()}";
   }
 }
--- test/unit/utils/extensions/date_time_test.dart
+++ test/unit/utils/extensions/date_time_test.dart
@@ -4,10 +4,10 @@ import 'package:productive_app/utils/extensions/datetime.dart';
 void main() {
   group("Date time Extension tests", () {
     test("test toStringYYYYMMDD with separator", () {
-      expect(DateTime(2010,11,15).toStringYYYYMMDD(separator:"-"), "2010-11-15");
-      expect(DateTime(2019,1,2).toStringYYYYMMDD(separator:"-"), "2019-01-02");
-      expect(DateTime(2020,1,2).toStringYYYYMMDD(separator:" "), "2020 01 02");
-      expect(DateTime(2020,10,2).toStringYYYYMMDD(separator:"-"), "2020-10-02");
+      expect(DateTime(2010,11,15).toStringYYYYMMDD(sep:"-"), "2010-11-15");
+      expect(DateTime(2019,1,2).toStringYYYYMMDD(sep:"-"), "2019-01-02");
+      expect(DateTime(2020,1,2).toStringYYYYMMDD(sep:" "), "2020 01 02");
+      expect(DateTime(2020,10,2).toStringYYYYMMDD(sep:"-"), "2020-10-02");
     });
   });
 }
